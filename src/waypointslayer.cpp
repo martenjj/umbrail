84bf13a12eb0fd250051d86f599c2395f5643820
Jonathan Marten <jjm@keelhaul.me.uk>
2017-12-18 18:09:34 +0000

    Painting of waypoint bearing lines

--- src/waypointslayer.cpp
+++ src/waypointslayer.cpp
@@ -12,6 +12,7 @@
 
 #include "settings.h"
 #include "mapview.h"
+#include "dataindexer.h"
 #include "trackdata.h"
 
 
@@ -30,6 +31,17 @@
 //////////////////////////////////////////////////////////////////////////
 
 static const int POINT_CIRCLE_SIZE = 20;		// size of selected circle
+
+// Earth circumference is 2*PI*radius where radius = 6371km
+//
+// This corresponds to an value of 2*PI for the 'distance' parameter
+// to GeoDataCoordinates::moveByBearing()
+//
+// The 2*PI factor cancels out, so this value gives a bearing line
+// of that length in kilometres.
+
+static const double BEARING_LINE_LENGTH_KM = 10;
+static const double BEARING_LINE_LENGTH = (BEARING_LINE_LENGTH_KM/6371.0);
 
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
@@ -92,7 +104,24 @@ void WaypointsLayer::doPaintItem(const TrackDataItem *item, GeoPainter *painter,
         GeoDataCoordinates coord(tdw->longitude(), tdw->latitude(),
                                  0, GeoDataCoordinates::Degree);
 
-        // First the selection marker
+        // First of all the bearing line, if there is one
+        const QString brg = tdw->metadata("bearingline");
+        if (!brg.isEmpty())
+        {
+            GeoDataCoordinates coord2 = coord.moveByBearing(DEGREES_TO_RADIANS(brg.toDouble()),
+                                                            BEARING_LINE_LENGTH);
+
+            // Using Marble::Tessellate gives a great circle line.
+            // Not a great difference at small scales, but
+            // better to have the best accuracy.
+            GeoDataLineString lineString(Marble::Tessellate);
+            lineString << coord << coord2;
+
+            painter->setPen(QPen(Qt::black, 2, Qt::DashLine));
+            painter->drawPolyline(lineString);
+        }
+
+        // Then the selection marker
         // Not "isSelected" - only for the selected waypoint, not the container
         if (tdw->selectionId()==mSelectionId)
         {
