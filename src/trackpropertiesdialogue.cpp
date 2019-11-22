martenjj/umbrail

commit 7de2471757eba320b61d33badca93a97d233b45f

Properties dialogue: Save state on "Close" (but not on "Cancel")

src/trackpropertiesdialogue.cpp
@@ -5,11 +5,14 @@
 #include <qlabel.h>
 #include <qdebug.h>
 #include <qtabwidget.h>
+#include <qpushbutton.h>
 
 #include <klocalizedstring.h>
 #include <kiconloader.h>
 #include <kconfiggroup.h>
 #include <kstandardguiitem.h>
+
+#include <dialogstatewatcher.h>
 
 #include "trackdata.h"
 #include "metadatamodel.h"
@@ -33,6 +36,8 @@ TrackPropertiesDialogue::TrackPropertiesDialogue(const QList<TrackDataItem *> *i
 
     setModal(true);
     setButtons(QDialogButtonBox::Ok|QDialogButtonBox::Close);
+    stateWatcher()->setSaveOnButton(buttonBox()->button(QDialogButtonBox::Close));
+    mCloseButtonShown = true;
 
     Q_ASSERT(!items->isEmpty());
     const TrackDataItem *item = items->first();
@@ -133,6 +138,7 @@ void TrackPropertiesDialogue::slotModelDataChanged(int idx)
 
     setButtonEnabled(QDialogButtonBox::Ok, ok);
     setButtonGuiItem(QDialogButtonBox::Close, KStandardGuiItem::cancel());
+    mCloseButtonShown = false;
 }
 
 
@@ -179,6 +185,13 @@ void TrackPropertiesDialogue::showEvent(QShowEvent *ev)
 
 void TrackPropertiesDialogue::saveConfig(QDialog *dialog, KConfigGroup &grp) const
 {
+    // Because of the setSaveOnButton(QDialogButtonBox::Close) used in
+    // the constructor, this will be called when that button is used
+    // regardless of whether it says "Close" or "Cancel".  So we use
+    // the flag to check what the button says, and only save state
+    // if it is still "Close".
+    if (!mCloseButtonShown) return;			// button is actually "Cancel"
+
     grp.writeEntry(QString("Index%1").arg(mItemType), mTabWidget->currentIndex());
     DialogStateSaver::saveConfig(dialog, grp);
 }

src/trackpropertiesdialogue.h
@@ -47,6 +47,7 @@ private:
     TrackData::Type mItemType;
     MetadataModel *mDataModel;
     QVector<bool> mPageDataChanged;
+    bool mCloseButtonShown;
 };
 
 #endif							// TRACKPROPERTIESDIALOGUE_H

commit 9fa1f389b0ac368ea33c1f777a09a77c939fd2f9

Properties dialogue: Correctly save state on "OK"
Problem introduced by commit 7de24717.

src/trackpropertiesdialogue.cpp
@@ -190,7 +190,10 @@ void TrackPropertiesDialogue::saveConfig(QDialog *dialog, KConfigGroup &grp) con
     // regardless of whether it says "Close" or "Cancel".  So we use
     // the flag to check what the button says, and only save state
     // if it is still "Close".
-    if (!mCloseButtonShown) return;			// button is actually "Cancel"
+    if (result()==QDialog::Rejected)			// "Close" or "Cancel" button
+    {
+        if (!mCloseButtonShown) return;			// button is currently "Cancel"
+    }
 
     grp.writeEntry(QString("Index%1").arg(mItemType), mTabWidget->currentIndex());
     DialogStateSaver::saveConfig(dialog, grp);